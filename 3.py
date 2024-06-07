17e571638cbe20581ca5a5ab378ebf99e77e5393
NotLSane <renato.carrasco2024@alu.uct.cl>
2024-06-07 18:59:01 -0400

    Pregunta 3 Base Code (Optimizaciones a corregir)
    Se agrega Data.py al archivo, modificando los datos para que concuerden con la base de datos, se agrega una función para la consulta SQL correspondiente, Prueba de Try y Except para que analice la tabla en X,Y.
    
    Cambios a realizar: Optimización para dato verificador rut, modificar o simplificar Try y Except

--- 3.py
+++ 3.py
@@ -1,25 +1,43 @@
 import sqlite3
 import pandas as pd
-import docx 
+import docx
 import Funciones as fn
-import pandas as pd
 from word_gen import example_contract
 
-start = fn.Conectar_db("db_personas.db")
+dato = (input("Ingresa el rut del usuario: "))
+
+doc =  docx.Document()
+start = fn.Conectar_P("db_personas.db")
+
 def singular_data_to_contract(df: pd.DataFrame, index_row:int):
     sub_df = df.iloc[index_row]
-    date = sub_df['Fecha']
-    rol = sub_df['Rol']
-    address = sub_df['Residencia']
-    rut = sub_df['RUT']
+    date = sub_df['fecha']
+    rol = sub_df['rol']
+    address = sub_df['residencia']
+    rut = sub_df['rut']
     full_name = sub_df['nombre_completo']
-    nationality = sub_df['Nacionalidad']
-    birth_date = sub_df['Fecha de nacimiento']
-    profession = sub_df['Profesion']
-    salary = sub_df['Sueldo']
+    nationality = sub_df['nacionalidad']
+    birth_date = sub_df['fecha de nacimiento']
+    profession = sub_df['profesion']
+    salary = sub_df['sueldo']
     example_contract(date, rol, address, rut, full_name, nationality, birth_date, profession, str(salary))
 
 
-#        Ejercicio 2
+def contrato_empleador(db_personas, employee_id):
+    df = start
+    print(df)
+    try:
+        index_row = df[df['rut'] == employee_id].index[0]
+        print(f"Empleado encontrado: {employee_id}, en la fila: {index_row}")
+    except IndexError:
+        print(f"Empleado con rut {employee_id} no encontrado.")
+        return
+    
+if __name__ == "__main__":
+    db = "db_personas.db"
+    employee_id = dato 
+    contrato_empleador(db, employee_id)
 
-doc =  docx.Document()
+
+
+
--- Codigo.py
+++ Codigo.py
@@ -19,6 +19,5 @@ N2 = pd.read_sql_query(""" SELECT P.fecha_ingreso AS Fecha, S.Rol, P.residencia,
                           P.nombre_completo, P.nacionalidad, P.fecha_de_nacimiento, p.profesion,
                           S.Sueldo FROM Salarios AS S, personas AS P """, start)
 
-
 print(N2)
 
