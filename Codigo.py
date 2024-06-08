4cb17401d1b7754c9075c25026170c93777d658e
NotLSane <renato.carrasco2024@alu.uct.cl>
2024-06-07 22:28:22 -0400

    Actualización de Funciones y código Principal
    La consulta 2, pasa a ser Función en manera de facilitar el filtro de la consulta 3 (Por probar)

--- Codigo.py
+++ Codigo.py
@@ -14,10 +14,7 @@ print(N1)
 
 print("Consulta 2")
 #Consulta N°2
-
-N2 = pd.read_sql_query(""" SELECT P.fecha_ingreso AS Fecha, S.Rol, P.residencia, P.rut,
-                          P.nombre_completo, P.nacionalidad, P.fecha_de_nacimiento, p.profesion,
-                          S.Sueldo FROM Salarios AS S, personas AS P """, start)
-
-print(N2)
+db = "db_personas.db"
+do = fn.Conectar_P(db) 
+print(do)
 
--- Funciones.py
+++ Funciones.py
@@ -12,7 +12,9 @@ def Conectar_db (db_personas):
 # Conecta la base de datos a la Tabla Personas
 def Conectar_P(db_personas):
     conn = sqlite3.connect(db_personas)
-    con = "SELECT rut, nombre_completo FROM personas"  #Define la tabla Personas como lectura (con = consulta)
+    con = """ SELECT P.fecha_ingreso AS Fecha, S.Rol, P.residencia, P.rut,
+                          P.nombre_completo, P.nacionalidad, P.fecha_de_nacimiento, p.profesion,
+                          S.Sueldo FROM Salarios AS S, personas AS P """  #Define la tabla Personas como lectura (con = consulta)
     df = pd.read_sql_query(con, conn)
     conn.close()
     return df
