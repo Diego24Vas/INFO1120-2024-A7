477ffafd7672e25714c00d8c4e40d800a56833e3
Diego24Vas <dvasquez2024@alu.uct.cl>
2024-06-09 01:09:09 -0400

    Ejecicio 5 terminado

--- BASE 5.py
+++ BASE 5.py
@@ -5,26 +5,10 @@ import Funciones as fn
 from word_gen import example_contract
 
 
-while True:
-    try:
-        Nums = int(input("Ingresa el rut del usuario (Sin digito Verificador): "))
-        strNum = str(Nums)
-        Dat = input("Ingresa el digito Verificador: ")
-        UDat = Dat.upper()
-        print(UDat)
-        SumDat = strNum + "-" + UDat
-        print(SumDat)
-        break
 
-    except ValueError:
-        print("Error: Ingresa un número válido para el RUT.")
-    except NameError:
-        print("Error: Se ha producido un error de nombre")
-
-def singular_data_to_contract(df, employee_id):
+def singular_data_to_contract(sub_df):
     try:
-        index_row = df[df["rut"] == employee_id].index[0]
-        sub_df = df.iloc[index_row]
+        index_row = sub_df.name
         print("Ya se creo el archivo")
         date = sub_df['fecha_ingreso']
         rol = sub_df['Rol']
@@ -35,13 +19,53 @@ def singular_data_to_contract(df, employee_id):
         birth_date = sub_df['fecha_de_nacimiento']
         profession = sub_df['profesion']
         salary = sub_df['Sueldo']
+        #Datos egresados, solo como comprobacion
+        print(f"Empleado encontrado: {rut}, en la fila: {index_row}")
+        print("Datos del empleado:")
+        print(f"Fecha: {sub_df['fecha_ingreso']}")
+        print(f"Rol: {sub_df['Rol']}")
+        print(f"Residencia: {sub_df['residencia']}")
+        print(f"RUT: {sub_df['rut']}")
+        print(f"Nombre Completo: {sub_df['nombre_completo']}")
+        print(f"Nacionalidad: {sub_df['nacionalidad']}")
+        print(f"Fecha de Nacimiento: {sub_df['fecha_de_nacimiento']}")
+        print(f"Profesión: {sub_df['profesion']}")
+        print(f"Sueldo: {sub_df['Sueldo']}")    
         example_contract(date, rol, address, rut, full_name, nationality, birth_date, profession, str(salary))
     except IndexError:
-        print(f"Empleado con rut {employee_id} no encontrado.")
+        print(f"Empleado con rut {rut} no encontrado.")
         return    
     
-db = "db_personas.db"
-start = fn.Conectar_P(db) 
-employee_id = SumDat #Asigna Employee_Id como confirmacion de RUT
-singular_data_to_contract(start, employee_id)
 
+
+# Funcion que llama los datos
+def range_usu(ini_rut, fin_rut):
+    try: 
+        conn = fn.Conectar_db("db_personas.db")
+        cons = f""" SELECT * FROM personas 
+                    NATURAL JOIN Salarios 
+                    WHERE rut BETWEEN {ini_rut} AND {fin_rut}"""
+        df = pd.read_sql_query(cons, conn)
+
+        # Comprueba si df esta vacio, si no genera los contratos
+        if df.empty:   
+            print("No se encontraron empleados en el rango especificado.")
+        else:
+            for index, row in df.iterrows():
+                singular_data_to_contract(row)
+        return df
+        
+    except Exception as e:
+        print(f"Error al conectar a la base de datos o al procesar datos: {e}")
+
+# Consulta al usuario
+if __name__ == "__main__":
+    while True:
+        try:
+            ini_rut = int(input("Ingresa el RUT inicial del rango (sin dígito verificador): "))
+            fin_rut = int(input("Ingresa el RUT final del rango (sin dígito verificador): "))
+            break
+        except ValueError:
+            print("Error: Ingresa un número válido para el RUT.")
+    
+    range_usu(ini_rut, fin_rut)
